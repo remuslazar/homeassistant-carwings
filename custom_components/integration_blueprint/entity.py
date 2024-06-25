9b3bd0b83bbb7bbb1e43a3c78fd662b3aa640a4c
Remus Lazar <remus@lazar.info>
2024-06-25 06:45:17 +0200

    Initial commit

---
+++ custom_components/integration_blueprint/entity.py
@@ -0,0 +1,31 @@
+"""BlueprintEntity class."""
+
+from __future__ import annotations
+
+from homeassistant.helpers.device_registry import DeviceInfo
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+
+from .const import ATTRIBUTION
+from .coordinator import BlueprintDataUpdateCoordinator
+
+
+class IntegrationBlueprintEntity(CoordinatorEntity[BlueprintDataUpdateCoordinator]):
+    """BlueprintEntity class."""
+
+    _attr_attribution = ATTRIBUTION
+
+    def __init__(self, coordinator: BlueprintDataUpdateCoordinator) -> None:
+        """Initialize."""
+        super().__init__(coordinator)
+        self._attr_unique_id = coordinator.config_entry.entry_id
+        self._attr_device_info = DeviceInfo(
+            identifiers={
+                (
+                    coordinator.config_entry.domain,
+                    coordinator.config_entry.entry_id,
+                ),
+            },
+            name=coordinator.config_entry.runtime_data.integration.name,
+            model=coordinator.config_entry.runtime_data.integration.version,
+            manufacturer=coordinator.config_entry.runtime_data.integration.name,
+        )
