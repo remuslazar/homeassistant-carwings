b9024f21c54f8486b1174f7782205f51f6b1ab5b
Tobias Laross <tobias.laross@gmail.com>
2025-02-07 20:13:00 +0100

    Exposed climate and charging as services

--- custom_components/nissan_carwings/__init__.py
+++ custom_components/nissan_carwings/__init__.py
@@ -11,10 +11,7 @@ from typing import TYPE_CHECKING
 
 from homeassistant.const import CONF_PASSWORD, CONF_REGION, CONF_USERNAME, Platform
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
-from homeassistant.helpers import config_validation as cv
-from homeassistant.exceptions import ServiceValidationError
-import voluptuous as vol
-
+from homeassistant.exceptions import HomeAssistantError
 from homeassistant.loader import async_get_loaded_integration
 
 from custom_components.nissan_carwings.const import (
@@ -25,6 +22,9 @@ from custom_components.nissan_carwings.const import (
     DOMAIN,
     LOGGER,
     SERVICE_UPDATE,
+    SERVICE_START_CLIMATE,
+    SERVICE_STOP_CLIMATE,
+    SERVICE_START_CHARGING,
 )
 
 from .api import NissanCarwingsApiClient
@@ -91,7 +91,10 @@ async def async_setup_entry(
     await coordinator.async_config_entry_first_refresh()
 
     climate_coordinator.data = {DATA_CLIMATE_STATUS_KEY: None, DATA_TIMESTAMP_KEY: None}
-    driving_analysis_coordinator.data = {DATA_DRIVING_ANALYSIS_KEY: None, DATA_TIMESTAMP_KEY: None}
+    driving_analysis_coordinator.data = {
+        DATA_DRIVING_ANALYSIS_KEY: None,
+        DATA_TIMESTAMP_KEY: None,
+    }
 
     # synchronize data in background to speedup the startup time for this integration
     # related entities will stick in the unavailable state until the first data is fetched
@@ -100,28 +103,7 @@ async def async_setup_entry(
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     entry.async_on_unload(entry.add_update_listener(async_reload_entry))
-
-    async def async_handle_update(service_call) -> None:
-        """Handle service to update leaf data from Nissan servers."""
-        vin = service_call.data["vin"]
-
-        LOGGER.debug("Service call to update data for VIN=%s", vin)
-
-        client = coordinator.config_entry.runtime_data.client
-        current_vin = coordinator.config_entry.data["vin"]
-        if vin != current_vin:
-            raise ServiceValidationError(
-                f"VIN mismatch: service call to update data for VIN={vin}, but current VIN={current_vin}"
-            )
-
-        # request the latest data from the Nissan servers
-        await client.async_update_data()
-        # tell the coordinator to refresh the data
-        await coordinator.async_request_refresh()
-
-    hass.services.async_register(
-        DOMAIN, SERVICE_UPDATE, async_handle_update, schema=vol.Schema({vol.Required("vin"): cv.string})
-    )
+    await register_services(hass, entry)
 
     return True
 
@@ -141,3 +123,66 @@ async def async_reload_entry(
     """Reload config entry."""
     await async_unload_entry(hass, entry)
     await async_setup_entry(hass, entry)
+
+
+async def register_services(hass: HomeAssistant, entry: NissanCarwingsConfigEntry):
+    """Register services for Nissan Carwings."""
+
+    async def async_handle_update(call):
+        """Handle service to update leaf data from Nissan servers."""
+
+        client = entry.runtime_data.client
+        coordinator = entry.runtime_data.coordinator
+        current_vin = entry.data["vin"]
+        LOGGER.debug("Service call to update data for VIN=%s", current_vin)
+
+        # request the latest data from the Nissan servers
+        await client.async_update_data()
+        # tell the coordinator to refresh the data
+        await coordinator.async_request_refresh()
+
+    async def start_climate_service(call):
+        """Handle starting the climate system."""
+        client = entry.runtime_data.client
+        await client.async_set_climate(switch_on=True)
+        entry.runtime_data.climate_coordinator.set_climate_pending_state(True)
+
+    async def stop_climate_service(call):
+        """Handle stopping the climate system."""
+        client = entry.runtime_data.client
+        await client.async_set_climate(switch_on=False)
+        entry.runtime_data.climate_coordinator.set_climate_pending_state(False)
+
+    async def start_charging(call):
+        """Handle starting charging."""
+        client = entry.runtime_data.client
+        try:
+            result = await client.async_start_charging()
+            if not result:
+                raise HomeAssistantError("Failed to start charging")
+        except Exception as exception:
+            raise HomeAssistantError(f"Error starting charging: {exception}")
+
+    hass.services.async_register(
+        DOMAIN,
+        SERVICE_UPDATE,
+        async_handle_update,
+    )
+
+    hass.services.async_register(
+        DOMAIN,
+        SERVICE_START_CLIMATE,
+        start_climate_service,
+    )
+
+    hass.services.async_register(
+        DOMAIN,
+        SERVICE_STOP_CLIMATE,
+        stop_climate_service,
+    )
+
+    hass.services.async_register(
+        DOMAIN,
+        SERVICE_START_CHARGING,
+        start_charging,
+    )
